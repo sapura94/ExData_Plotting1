f312bc20ebe3a40889c64fb3d4eb2f50b363443a
sapura94 <34736995+sapura94@users.noreply.github.com>
2018-09-14 13:35:14 +0800

    Plot2.r

---
+++ Plot2.r
@@ -0,0 +1,6 @@
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)")
+
